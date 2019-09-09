b90f89fa4c09295c43465e8c22dc62a94be9bb37
call900913 <call900913@icloud.com>
2019-09-09 13:35:52 -0400

    Complete view for root url

--- src/ListShelves.js
+++ src/ListShelves.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import CurrentlyReadingShelf from './CurrentlyReadingShelf';
-import WantToReadShelf from './WantToReadShelf';
-import HaveReadShelf from './HaveReadShelf';
-
-class ListShelves extends Component {
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <CurrentlyReadingShelf />
-        <WantToReadShelf />
-        <HaveReadShelf />
-        <div className="open-search">
-          <Link to='/search'>Add a book</Link>
-        </div>
+import Shelf from './Shelf';
+const ListShelves = props => {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    )
-  }
+    {Object.keys(props.shelves).map((obj) => (
+      <Shelf key={obj} heading={obj} books={props.shelves[obj]} handleChange={props.handleChange}/>
+    ))}
+      <div className="open-search">
+        <Link to='/search'>Add a book</Link>
+      </div>
+    </div>
+  )
 }
-
 export default ListShelves;
