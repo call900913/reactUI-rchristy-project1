b1dace50436a272dbde28b98b5cb752ceb8a6635
call900913 <call900913@icloud.com>
2019-09-09 13:37:21 -0400

    Complete Search page Component

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI.js';
+import Book from './Book';
 
 class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      query: '',
+      searchResults: []
     }
   }
 
+  onChange = (q) => {
+    this.setState((prevState) => ({
+      query: q
+    }))
+    BooksAPI.search(q).then((stuff) => {
+      const addedbooks = []
+      stuff.forEach((item) => {
+        addedbooks.push(item.id);
+      })
+      this.setState((prevState) => ({
+        searchResults: addedbooks
+      }))
+      console.log('addedbooks: ' + addedbooks)
+      console.log('search results: ', this.state.searchResults);
+    })
+  }
+
   render() {
+    const { searchResults, query } = this.state;
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -17,20 +38,20 @@ class SearchPage extends Component {
             to='/'
             className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(evt) => this.onChange(evt.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {searchResults.map((id) => (
+              <Book key={id} id={id} handleChange={this.props.handleChange}/>
+            ))}
+          </ol>
         </div>
       </div>
     )
