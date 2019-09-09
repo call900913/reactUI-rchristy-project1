37de8e087c4facb7dbd380afd792679b978911cd
call900913 <call900913@icloud.com>
2019-09-09 13:30:15 -0400

    Complete src/App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,78 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import ListShelves from './ListShelves';
 import SearchPage from './SearchPage';
+import * as BooksAPI from './BooksAPI.js';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentlyReading: [],
+      wantToRead: [],
+      read: []
+    }
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((result) => {
+      console.log('get all result: ', result);
+      const a1 = [];
+      const a2 = [];
+      const a3 = [];
+      result.forEach((item) => {
+        if (item.shelf === 'currentlyReading') {
+          a1.push(item.id);
+        } else if (item.shelf === 'wantToRead') {
+          a2.push(item.id);
+        } else if (item.shelf === 'read'){
+          a3.push(item.id);
+        }
+      })
+      this.setState((prevState) => ({
+        currentlyReading: a1,
+        wantToRead: a2,
+        read: a3
+      }))
+    })
+  }
+
+  handleChange = (book, shelf) => {
+    console.log('entering');
+    console.log('book: ', book);
+    console.log('shelf: ', shelf);
+    BooksAPI.update({ id: book }, shelf);
+    console.log('exiting');
+    BooksAPI.getAll().then((result) => {
+      console.log('get all result: ', result);
+      const a1 = [];
+      const a2 = [];
+      const a3 = [];
+      result.forEach((item) => {
+        if (item.shelf === 'currentlyReading') {
+          a1.push(item.id);
+        } else if (item.shelf === 'wantToRead') {
+          a2.push(item.id);
+        } else if (item.shelf === 'read') {
+          a3.push(item.id);
+        }
+      })
+      this.setState((prevState) => ({
+        currentlyReading: a1,
+        wantToRead: a2,
+        read: a3
+      }))
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <ListShelves />
+          <ListShelves shelves={this.state} handleChange={this.handleChange}/>
         )}/>
         <Route exact path='/search' render={() => (
-          <SearchPage />
+          <SearchPage handleChange={this.handleChange}/>
         )}/>
       </div>
     )
